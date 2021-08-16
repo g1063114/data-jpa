5755b0b67705c074d041c2b212955a69f7315f24
jinho <g1063114@naver.com>
2021-08-16 21:33:59 +0900

    data-jpa bulk update query

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import study.datajpa.dto.MemberDto;
@@ -38,4 +39,8 @@ public interface MemberRepository extends JpaRepository<Member,Long> {
     // paging
     @Query(value = "select m from Member m", countQuery = "select count(m) from Member m")
     Page<Member> findByAge(int age, Pageable pageable);
+
+    @Modifying(clearAutomatically = true)   // db 쿼리 나가고 나서 자동으로 영속성 컨텍스트 클리어
+    @Query("update Member m set m.age = m.age + 1 where m.age >= :age")
+    int bulkAgePlus(@Param("age") int age);
 }
